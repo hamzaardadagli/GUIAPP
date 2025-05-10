0b2529fcc0fc2891a7886e36b4e10d3c66bb5250
hamzaardadagli <hamzaardadagli07@gmail.com>
2025-05-10 20:25:55 +0300

    GUI APP
    GUI APP EXAMPLES

---
+++ GUI_App/src/guı_app/JOpitonPane.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package guı_app;
+
+/**
+ *
+ * @author HP
+ */
+class JOpitonPane {
+
+    static void showMessageDialog(FirstApp7 aThis, String sayin_Yönetici_Hoşgeldiniz) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+    
+}
---
+++ GUI_App/src/metin.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author HP
+ */
+class metin {
+
+    static void add(String text) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    static void remove(String text) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+    
+}
